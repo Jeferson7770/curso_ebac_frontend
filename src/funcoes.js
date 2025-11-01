71f5d6851614731b48983579562d1d573493a466
jeferson7770 <jefersonmoreira770@gmail.com>
2025-11-01 10:33:16 -0300

    initial commit

---
+++ src/funcoes.js
@@ -0,0 +1,8 @@
+function multiplica(n1, n2) {
+    return n1 * n2;
+}
+console.log(multiplica(9, 3)); // Saída: 27
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(saudacao("Jeferson")); // Saída: Olá Jeferson
---
+++ src/funcoes.ts
@@ -0,0 +1,11 @@
+function multiplica(n1: number, n2: number): number {
+    return n1 * n2;
+}
+
+console.log(multiplica(9, 3)); // Saída: 27
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(saudacao("Jeferson")); // Saída: Olá Jeferson
